3f6c1c6282332ae96b2f2cf53627dc580eed2ce2
antonioducs <antonioduarte.cs@gmail.com>
2019-11-14 08:56:29 -0300

    connect wifi

--- include/main.h
+++ include/main.h
@@ -1,4 +1,5 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
-#define LED 14 //D5
+#define LED_GREEN 14 //D5
+#define LED_RED 12 //D6
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,25 @@
 #include "main.h"
 
+WiFiServer server(81);
+
 void setup() {
-  pinMode(LED, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_GREEN, LOW);
+
+  WiFi.begin("Monitora", "monitora123");
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+  }
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  delay(1000);
-  digitalWrite(LED, LOW);
-  delay(1000);
+  
 }
